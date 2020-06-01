a6398483f2733e3fdc74c7f1d46b303ae369e54f
cleysonl <leysonl@gmail.com>
2020-06-01 15:32:06 -0500

    Fixed names of variables

--- Python/main_vf.py
+++ Python/main_vf.py
@@ -14,7 +14,7 @@
 from MCGDM import *
 from Wellbeing import *
 from Health import *
-from Security import *
+# from Security import *
 from FCS import *
 import pandas as pd
 from fuzzycreator.measures.similarity_t1 import jaccard
@@ -23,41 +23,42 @@ from fuzzycreator.measures.similarity_t1 import jaccard
 
 inputs_wellbeing =['Visits','Mind Exercise','FamilyInt']
 inputs_health =['Medicine','Physical Exercise','Ext Temperature']
-inputs_security =['Location','Mobility','Environment']
+# inputs_security =['Location','Mobility','Environment']
 
 # Remove comment # for aspect analyzed (Wellbeing or Health)
-# in_values_wellbeing = {"num_visits": 14,"t_exercise": 30,"ext_temp": 25}  #BLUE
-in_values_wellbeing = {"num_visits": 7.05/30 ,"t_exercise": 0.5,"ext_temp": 13.2/30}  
-# in_values_health = {"med": 0.91,"press": 0.102,"b_temp": 0.657}
+# in_values_wellbeing = {"visits": 14,"mind_exercise": 30,"familyint": 25}  #BLUE
+in_values_wellbeing = {"visits": 7.05/30 ,"mind_exercise": 0.5,"familyint": 13.2/30}  
+in_values_health = {"med": 0.91,"p_exercise": 0.102,"temp": 0.657}
 # in_values_security = {"loc": 0.5,"mob": 3,"env": 2.5}
 
 # Reads the rules from an excel file
-rulesw = pd.read_excel('rules.xlsx', sheet_name='Wellbeing')
-rulesh = pd.read_excel('rules.xlsx', sheet_name='Health')
-ruless = pd.read_excel('rules.xlsx', sheet_name='Security')
+rulesw = pd.read_excel(r'Python\rules.xlsx', sheet_name='Wellbeing')
+rulesh = pd.read_excel(r'Python\rules.xlsx', sheet_name='Health')
+# ruless = pd.read_excel('rules.xlsx', sheet_name='Security')
 
 # Ways to separate outputs and inputs from the rules database
 
 rules_inpw = rulesw[['Visits','Mind Exercise','FamilyInt']]
 rules_inph = rulesh[['Medicine','Physical Exercise','Ext Temperature']]
-rules_inps = ruless[['Location','Mobility','Environment']]
+# rules_inps = ruless[['Location','Mobility','Environment']]
 rules_outw = rulesw[['Colors']]
-rules_outh = rulesh[['Colors']]
-rules_outs = ruless[['Colors']]
+# rules_outh = rulesh[['Colors']]
+# rules_outs = ruless[['Colors']]
 
 
 # Remove comments depending on which aspect wants to be evaluated
 ## Wellbeing
-in_valueswb = Wellbeing(in_values_wellbeing["num_visits"],
-              in_values_wellbeing["t_exercise"],
-              in_values_wellbeing["ext_temp"])
+in_valueswb = Wellbeing(in_values_wellbeing["visits"],
+              in_values_wellbeing["mind_exercise"],
+              in_values_wellbeing["familyint"])
+
 print(in_valueswb.mv_wellbeing())
 
 ## Health
-# in_valuesh = Health(in_values_health["med"],
-#               in_values_health["press"],
-#               in_values_health["b_temp"])
-# print(in_valuesh.mv_health())
+in_valuesh = Health(in_values_health["med"],
+              in_values_health["p_exercise"],
+              in_values_health["temp"])
+print(in_valuesh.mv_health())
 
 ## Security
 # in_valuess = Security(in_values_security["loc"],
@@ -71,9 +72,10 @@ print(in_valueswb.mv_wellbeing())
 # wellbeing= MCGDM(inputs_wellbeing, rules_inpw, in_valueswb.mv_wellbeing(), rules_outw)
 # print(wellbeing.weight()[0])
 # print(wellbeing.weight()[1])
-wellbeing= FCS(inputs_wellbeing, rules_inpw, in_valueswb.mv_wellbeing(), rules_outw)
-print(wellbeing.defuzzification())
-print(wellbeing.color())
+
+# wellbeing= FCS(inputs_wellbeing, rules_inpw, in_valueswb.mv_wellbeing(), rules_outw)
+# print(wellbeing.defuzzification())
+# print(wellbeing.color())
 
 ## Health ###
 # health= MCGDM(inputs_health, rules_inph, in_valuesh.mv_health(), rules_outh)
